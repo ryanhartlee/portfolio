aca6ba30bcb36bfa77dadef43d727f4bbc55a8f6
Ryan Hartley <ryanhartlee@gmail.com>
2019-02-18 15:45:55 -0700

    added skills

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,23 @@
+$('#css').hover(function (){
+    $('#css').tooltip('show');
+});
+
+$('#js').hover(function (){
+    $('#js').tooltip('show');
+});
+
+$('#node').hover(function (){
+    $('#node').tooltip('show');
+});
+
+$('#db').hover(function (){
+    $('#db').tooltip('show');
+});
+
+$('#react').hover(function (){
+    $('#react').tooltip('show');
+});
+
+$('#git').hover(function (){
+    $('#git').tooltip('show');
+});
